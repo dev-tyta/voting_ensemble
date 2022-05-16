be473379ba28804a130878587d2ef745ec1b1c4e
dev-tyta <hardeykoryar7@gmail.com>
2022-05-16 13:40:04 +0100

    input for loan prediction

--- main.py
+++ main.py
@@ -9,3 +9,26 @@ unique_mapping = pickle.load(map_pickle)
 
 st.title('Loan Availability Prediction')
 st.write('Intro...... ')
+
+gender = st.selectbox('Pick your Gender:', ['Female', 'Male'])
+
+married = st.selectbox('Marital Status:', ['Married', 'Single'])
+
+dependent = st.selectbox('Are you dependent on anyone:', ['Yes', 'No'])
+
+education = st.selectbox('Educational Background:', ['Graduate', 'Non-Graduate'])
+
+employed = st.selectbox('Employment Status:', ['Self-Employed', 'Employed'])
+
+base_currency = st.selectbox('Base Currency:', ['NGN', 'USD', 'EURO'])
+
+app_income = st.number_input('Applicant Income:')
+
+if base_currency == 'NGN':
+    dol_income = app_income / 570
+elif base_currency == 'USD':
+    dol_income = app_income
+elif base_currency == 'EURO'
+
+# Gender, Married, Dependents, Education, Self_Employed, ApplicantIncome,\
+# CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History, Property_Area
